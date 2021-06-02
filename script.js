6a8573af8adce8d2b27d004dc0cce04e39614c8e
stanochka <krozzochka@gmail.com>
2021-06-02 20:13:29 +0300

    Write function playRound

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ const computerPlay = () => {
       return 'Scissors';
   }
 }
+//plays a single round of Rock Paper Scissors
+const playRound = (playerSelection, computerSelection) => {
+  playerSelection = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+  let status;
+
+  if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+    status = true;
+  } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+    status = false;
+  } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+    status = true;
+  } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+    status = false;
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+    status = true;
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+    status = false;
+  } else {
+    return playRound();
+  }
+
+  if (status) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`
+  }
+}
