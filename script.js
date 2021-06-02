616acf5a7509716ea87339395a9bd86b5347b387
stanochka <krozzochka@gmail.com>
2021-06-02 21:48:08 +0300

    Fix function playRound, add function game, playerPlay

--- script.js
+++ script.js
@@ -10,30 +10,58 @@ const computerPlay = () => {
       return 'Scissors';
   }
 }
-//plays a single round of Rock Paper Scissors
-const playRound = (playerSelection, computerSelection) => {
-  playerSelection = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();
-  let status;
 
-  if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-    status = true;
-  } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-    status = false;
-  } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-    status = true;
-  } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-    status = false;
-  } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
-    status = true;
-  } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
-    status = false;
+//will prompt player to make a move
+const playerPlay = () => {
+	let playerText = prompt('Please enter Rock, Paper or Scissors:');
+  let playerSelection = playerText.slice(0, 1).toUpperCase() + playerText.slice(1).toLowerCase();
+  const valid = ['Rock', 'Paper', 'Scissors'];
+  if (valid.includes(playerSelection)) {
+  	return playerSelection;
   } else {
-    return playRound();
-  }
-
-  if (status) {
-    return `You Win! ${playerSelection} beats ${computerSelection}`
-  } else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`
+  alert('Invalid input!');
+  	return playerPlay();
   }
 }
+
+//plays a single round of Rock Paper Scissors
+const playRound = (playerSelection, computerSelection) => {
+	console.log('YOU: ' + playerSelection + ', COMPUTER: ' + computerSelection);
+  if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    return 1;
+  } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    return 0;
+  } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    return 1;
+  } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    return 0;
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    return 1;
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    return 0;
+  } else if (playerSelection === computerSelection) {
+  	console.log('It\'s a tie! Play another round');
+  	return playRound(playerPlay(), computerPlay());
+  }
+}
+
+const game = () => {
+  let results = [];
+  while (results.length < 5) {
+    let round = playRound(playerPlay(), computerPlay());
+    results.push(round);
+  }
+  if (results.reduce((sum, x) => sum + x) >= 3) {
+    return console.log('YOU WIN!');
+  } else {
+    return console.log('YOU LOOSE:(')
+  }
+}
+
+game()
